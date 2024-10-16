45113f390aec32a4e5076aafbe52248c306e8a9f
Xing Da <xingda@u.nus.edu>
2024-10-16 23:14:57 +0800

    merge client/server codes together

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,11 @@ import * as vscode from "vscode";
 import { exec } from "child_process";
 import * as path from "path";
 import { spawn, ChildProcessWithoutNullStreams } from "child_process";
+import * as Client from "scp2"; //For copying files
+import * as fs from "fs";
+import * as spawnConnection from "./commands/spawnConnection";
+import { runShell } from "./commands/utils";
 
-const filePath = path.join(__dirname, "..", "src", "test.sh");
 const vagrantPath = path.join(
   __dirname,
   "..",
@@ -54,22 +57,6 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-
-  let runShell = vscode.commands.registerCommand(
-    "extension.runShellScript",
-    () => {
-      const bashPath = "D:\\Git\\usr\\bin\\bash.exe";
-      // Run shell script with git bash
-      exec(`"${bashPath}" "${filePath}"`, (error, stdout, stderr) => {
-        if (error) {
-          vscode.window.showErrorMessage(`Error: ${stderr}`);
-          // vscode.window.showErrorMessage(`Error: ${filePath}`);
-        } else {
-          vscode.window.showInformationMessage(`Output: ${stdout}`);
-        }
-      });
-    }
-  );
 
   let vagrantUp = vscode.commands.registerCommand(
     "extension.runVagrant",
@@ -124,6 +111,50 @@ export function activate(context: vscode.ExtensionContext) {
     });
   }
 
+  const activateCommand = vscode.commands.registerCommand(
+    "extension.activatePlugin",
+    async () => {
+      //connect to SSH
+      createTerminal();
+      vscode.window.showInformationMessage("SSH connection established.");
+      sendCommandToTerminal("ls");
+      sendCommandToTerminal("cd tests");
+      sendCommandToTerminal("ls");
+      // Prompt for Project
+      const outputChannel = vscode.window.createOutputChannel("SSH Output");
+      outputChannel.show(true);
+
+      vscode.window.showInformationMessage("Im Waiting");
+      const project = await vscode.window.showInputBox({
+        prompt: "Enter the project to run rulekeeper",
+        placeHolder: "Project Name",
+        ignoreFocusOut: true,
+      });
+
+      if (project !== undefined && project.trim() !== "") {
+        vscode.window.showInformationMessage("hehe");
+        try {
+          const output = await spawnConnection.runCommandAndCaptureOutput(
+            identityFile,
+            `cd tests && ./setup.sh ${project}`
+          );
+          vscode.window.showInformationMessage(`Setup output: ${output}`);
+          //Wait for command to be completed
+          const completionIndicator =
+            "[INFO] - Running container neo4j-rulekeeper";
+          outputChannel.append(output);
+          if (output.includes(completionIndicator)) {
+            //Send enter to terminal
+            sendCommandToTerminal("");
+            sendCommandToTerminal("cd webus");
+          }
+        } catch (error) {
+          vscode.window.showErrorMessage(`Error: ${error}`);
+        }
+      }
+    }
+  );
+
   // command for persistentVagrantSsh
   const connectCommand = vscode.commands.registerCommand(
     "extension.persistentVagrantSsh",
@@ -146,9 +177,112 @@ export function activate(context: vscode.ExtensionContext) {
         .then((project) => {
           if (project) {
             sendCommandToTerminal(`./setup.sh ${project}`);
-            sendCommandToTerminal(`./run.sh ${project}`);
           }
         });
+    }
+  );
+
+  function getVagrantfileDirectory(): string | null {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+
+    if (workspaceFolders && workspaceFolders.length > 0) {
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      const vagrantfilePath = path.join(rootPath, "Vagrantfile");
+
+      // Check if the Vagrantfile exists
+      if (fs.existsSync(vagrantfilePath)) {
+        return rootPath; // Return the directory containing the Vagrantfile
+      } else {
+        vscode.window.showErrorMessage(
+          "Vagrantfile not found in the workspace."
+        );
+        return null;
+      }
+    } else {
+      vscode.window.showErrorMessage("No workspace is open.");
+      return null;
+    }
+  }
+
+  function copyFileToVagrantDirectory(localPath: string, remotePath: string) {
+    vscode.window.showInformationMessage("Hello...Im trying to copy now");
+    if (!fs.existsSync(localPath)) {
+      vscode.window.showErrorMessage("Local file does not exist.");
+      return;
+    } else {
+      vscode.window.showErrorMessage("Local file exist.");
+    }
+    const config = {
+      host: "127.0.0.1",
+      port: 2222, // Vagrant SSH default port
+      username: "vagrant",
+      privateKey: require("fs").readFileSync(
+        "/Users/KY/.vagrant.d/insecure_private_keys/vagrant.key.rsa"
+      ), // Path to private key
+    };
+
+    //handling space in local Path
+    localPath = `${localPath}`;
+    console.log(localPath);
+
+    Client.scp(
+      localPath,
+      {
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        privateKey: config.privateKey,
+        path: remotePath,
+      },
+      (err: any) => {
+        vscode.window.showErrorMessage("SCP Callback reached");
+        if (err) {
+          vscode.window.showErrorMessage(`Failed to copy file: ${err.message}`);
+        } else {
+          vscode.window.showInformationMessage(`File copied to ${remotePath}`);
+        }
+      }
+    );
+  }
+
+  //getting vagrant config
+  function getVagrantSSHConfig(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      exec("vagrant ssh-config", (error, stdout, stderr) => {
+        if (error) {
+          reject(`Error getting Vagrant SSH config: ${stderr}`);
+        } else {
+          // Parse the SSH config output
+          const sshConfig: any = {};
+          stdout.split("\n").forEach((line) => {
+            const [key, value] = line.trim().split(" ");
+            if (key && value) {
+              sshConfig[key] = value;
+            }
+          });
+          resolve(sshConfig);
+        }
+      });
+    });
+  }
+
+  const copyCommand = vscode.commands.registerCommand(
+    "extension.copyToRemote",
+    () => {
+      const destinationPath = "/home/vagrant/tests/";
+      vscode.window
+        .showInputBox({
+          prompt: "Enter the local path to your project",
+        })
+        .then((localPath) => {
+          if (localPath) {
+            const folder = path.basename(localPath);
+            const finalpath = path.join(destinationPath, folder, path.sep);
+            console.log(finalpath);
+            copyFileToVagrantDirectory(localPath, finalpath);
+          }
+        });
+      vscode.window.showInformationMessage("Hello...Im trying");
     }
   );
 
@@ -156,6 +290,8 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vagrantUp);
   context.subscriptions.push(connectCommand);
   context.subscriptions.push(runRuleKeeperOnProject);
+  context.subscriptions.push(copyCommand);
+  context.subscriptions.push(activateCommand);
 }
 
 // This method is called when your extension is deactivated
